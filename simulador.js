25518e162fdc0687566b5be64d551918f79e3a89
mpaulamamede <mpaulamamede@gmail.com>
2021-09-29 19:05:29 -0300

    commit

---
+++ simulador.js
@@ -0,0 +1,34 @@
+function simular() {
+    var prazo = document.getElementById("prazo").valueAsNumber;
+    var jurosaa = document.getElementById("jurosaa").valueAsNumber;
+    var valor = document.getElementById("valor").valueAsNumber;
+
+    var prestacoes = prazo * 12;
+    var jurosam = ((1+jurosaa) ** (1/12)) - 1;
+    var amortizacao = valor/prestacoes;
+
+    document.getElementById("prestacoes").valueAsNumber = prestacoes;
+    document.getElementById("jurosam").valueAsNumber = jurosam;
+
+    var tbody = document.querySelector("tbody");
+    for(var i=0; i<5; i++) {
+        var saldoDevedor = valor - (i*amortizacao);
+        var jurosPrestacao = saldoDevedor*jurosam;
+        var totalPrestacao = jurosPrestacao + amortizacao;
+
+        var tr = tbody.children[i];
+        tr.children[1].textContent = amortizacao.toFixed(2);
+        tr.children[2].textContent = jurosPrestacao.toFixed(2);
+        tr.children[3].textContent = totalPrestacao.toFixed(2);    
+    }
+
+    var jurosTotal = 0;
+    for(var i=0; i<prestacoes; i++) {
+        var saldoDevedor = valor - i*amortizacao;
+        var jurosPrestacao = saldoDevedor*jurosam;
+        jurosTotal += jurosPrestacao;           
+    }
+    document.getElementById("jurostotal").valueAsNumber = jurosTotal.toFixed(2);
+}
+
+//simular();
